2a8a592da98b85188a589493bf1d4d334da29a0c
Sibongiseni Mxinwa <sibongiseni.mxinwa@gmail.com>
2025-03-12 13:29:10 +0200

    Routing in React using React Router

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react"
- import { Product } from "../models/product";
- import Catalog from "../../features/catalog/Catalog";
+import { useState } from "react"
  import { Box, Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
  import NavBar from "./NavBar";
+ import { Outlet } from "react-router-dom";
  
  function App() {
-   const [products, setProducts] = useState<Product[]>([]);
+   
    const [darkMode, setDarkMode] = useState(false);
    const palleteType = darkMode ? 'dark' : 'light'
    const theme = createTheme({
@@ -20,12 +19,7 @@ import { useEffect, useState } from "react"
    const toggleDarkMode = () => {
      setDarkMode(!darkMode);
    }
- 
-   useEffect(() => {
-     fetch('https://localhost:5001/api/products')
-       .then(response => response.json())
-       .then(data => setProducts(data));
-   }, [])
+
  
    return (
      <ThemeProvider theme={theme}>
@@ -41,7 +35,7 @@ import { useEffect, useState } from "react"
          }}
        >
          <Container maxWidth='xl' sx={{ mt: 8 }}>
-           <Catalog products={products} />
+         <Outlet />
          </Container>
        </Box>
  
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,14 +1,19 @@
-import { Product } from "../../app/models/product"
- import ProductList from "./ProductList";
- 
- type Props = {
-   products: Product[]
- }
- 
- export default function Catalog({products}: Props) {
-   return (
-     <>
-       <ProductList products={products} />
-     </>
-   )
- }
+import { useEffect, useState } from "react";
+import { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
+
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <>
+      <ProductList products={products} />
+    </>
+  );
+}
